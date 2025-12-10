4d20e41864e0c3201908182897549d4add21ffd3
cold2005 <2210140553@qq.com>
2025-12-10 16:06:46 +0800

    顺序表头文件接口声明

--- DataStructure/seqlist.h
+++ DataStructure/seqlist.h
@@ -9,8 +9,24 @@ typedef struct{
     int Maxsize;
 }Seqlist;
 
-bool Initlist(Seqlist*L,int Initsize);
+bool Initlist(Seqlist*L);
 
 bool InsertData(Seqlist*L,int pos,int data);
+
+bool DeleteDataPos(Seqlist*L,int pos);
+
+bool DeleteDataVal(Seqlist*L,int val);
+
+void SearchBypos(Seqlist*L,int pos);
+
+void SearchByval(Seqlist* L,int val);
+
+bool ChangeDataByPos(Seqlist* L,int newdata,int pos);
+
+bool ChangeDataByVal(Seqlist*L , int newdata, int val);
+
+void Printlist(Seqlist* L);
+
+
 
 #endif
