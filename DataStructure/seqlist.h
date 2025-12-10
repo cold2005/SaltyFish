997d4fa01a7015705b44a738412b4e40d5410d0b
cold2005 <2210140553@qq.com>
2025-12-10 16:25:44 +0800

    重新命名了一下

--- DataStructure/seqlist.h
+++ DataStructure/seqlist.h
@@ -9,24 +9,25 @@ typedef struct{
     int Maxsize;
 }Seqlist;
 
-bool Initlist(Seqlist*L);
+bool Seqlist_Initlist(Seqlist*L);
 
-bool InsertData(Seqlist*L,int pos,int data);
+bool Seqlist_InsertData(Seqlist*L,int pos,int data);
 
-bool DeleteDataPos(Seqlist*L,int pos);
+bool Seqlist_DeleteDataPos(Seqlist*L,int pos);
 
-bool DeleteDataVal(Seqlist*L,int val);
+bool Seqlist_DeleteDataVal(Seqlist*L,int val);
 
-void SearchBypos(Seqlist*L,int pos);
+void Seqlist_SearchBypos(Seqlist*L,int pos);
 
-void SearchByval(Seqlist* L,int val);
+void Seqlist_SearchByval(Seqlist* L,int val);
 
-bool ChangeDataByPos(Seqlist* L,int newdata,int pos);
+bool Seqlist_ChangeDataByPos(Seqlist* L,int newdata,int pos);
 
-bool ChangeDataByVal(Seqlist*L , int newdata, int val);
+bool Seqlist_ChangeDataByVal(Seqlist*L , int newdata, int val);
 
-void Printlist(Seqlist* L);
+void Seqlist_Printlist(Seqlist* L);
 
+void Seqlist_DropList(Seqlist* L);
 
 
 #endif
